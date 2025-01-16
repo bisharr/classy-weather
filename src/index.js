db33c3b33e3bd255ec770e628c5cb7e8b6cd640b
Bishar <bishar995@gmail.com>
2025-01-16 22:47:59 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Counter from "./Counter";
+
+// import Counter from "./Counter";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>{/* <Counter /> */}</React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
